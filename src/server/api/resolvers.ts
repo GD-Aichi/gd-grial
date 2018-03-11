ed9402842ab74a9c7a84be32fccf07ded9534691
Aichi <jens.kolberg@gamesdynamics.com>
2018-03-11 10:23:44 +0100

    change

--- src/server/api/resolvers.ts
+++ src/server/api/resolvers.ts
@@ -1,11 +1,13 @@
 import { resolve } from 'path';
+import { fileLoader, mergeResolvers } from 'merge-graphql-schemas';
 
 export const getResolvers = async (BASE_PATH: string) => {
   try {
-    return require(resolve(`${BASE_PATH}/resolvers`));
+    const typesArray = fileLoader(resolve(`${BASE_PATH}/resolvers`));
+    return mergeResolvers(typesArray);
   } catch (error) {
     if (error.code === 'MODULE_NOT_FOUND') {
-      throw new ReferenceError('The file `./resolvers.ts` or `./resolvers/index.ts` is required.');
+      throw new ReferenceError('The file(s) `./resolvers/` is required.');
     }
     throw error;
   }
--- typings.d.ts
+++ typings.d.ts
@@ -1,5 +1,5 @@
 declare module 'merge-graphql-schemas' {
-  export function fileLoader(folderPath: string, options: any): string[];
-  export function mergeTypes(types: string[], options: any): string;
-  export function mergeResolvers(types: string[], options: any): string;
+  export function fileLoader(folderPath: string, options?: any): string[];
+  export function mergeTypes(types: string[], options?: any): string;
+  export function mergeResolvers(types: any[], options?: any): any;
 }
